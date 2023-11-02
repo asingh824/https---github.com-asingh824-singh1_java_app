d8d4bd7ac314aa68facdb1d95ca55ed2746c6970
asingh824 <asingh91021@gmail.com>
2023-11-02 16:55:47 -0700

    git checkout stage added

---
+++ src/main/java/com/minikube/sample/MinikubeSampleApllication.java
@@ -0,0 +1,5 @@
+package main.java.com.minikube.sample;
+
+public class MinikubeSampleApllication {
+    
+}
---
+++ src/main/java/com/minikube/sample/properties/PropertiesConfig.java
@@ -0,0 +1,5 @@
+package main.java.com.minikube.sample.properties;
+
+public class PropertiesConfig {
+    
+}
